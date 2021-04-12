87b661d796a0e5189d83a6b9137f91720e9f208a
shanimergui <shanichai98@gmail.com>
2021-04-12 18:21:14 +0300

    finish

--- Board.cpp
+++ Board.cpp
@@ -6,12 +6,6 @@ namespace ariel
 {
 void Board::post(uint row, uint col, ariel::Direction d, const std::string & str)
 {
-const int min_row =1;
-const int max_row=2;
-const int min_row=3;
-const int max_row=4;
-
-
 this->_size[min_row] =  min(this->_size[min_row],row);
 this->_size[max_row] = max(this->_size[max_row],row);
 this->_size[min_row] =  min(this->_size[min_row],col);
@@ -47,14 +41,11 @@ return answer;
 void Board::show()
 {
 const uint name=10;
-const int min_row=1;
-const int max_row=2;
-const int min_row=3;
-const int  max_row=4;
+
 
 string answer;
 for (uint i = this->_size[min_row]-name; i < this->_size[max_row]+name; i++) {
-for (uint j = this->_size[min_row]-name; j < this->_size[ max_row]+name; j++) {
+for (uint j = this->_size[min_col]-name; j < this->_size[ max_col]+name; j++) {
         if(this->board[i][j]!='\0') {
                 answer+=this->board[i][j];
         }else{
--- Board.hpp
+++ Board.hpp
@@ -4,24 +4,31 @@
 using namespace std;
 namespace ariel
 {
-    class Board
-    {
+class Board
+{
 
-    private:
-      // key:1 -> min_row key:2 -> max_row
-      // key:3 -> min_row key:4 -> max_row
-      unordered_map<uint,uint> _size;
-      unordered_map<uint,unordered_map<uint,char>> board;
+private:
+// key:1 -> min_row     key:2 -> max_row
+// key:3 -> min_row     key:4 -> max_row
 
-        public:
-            Board(){
-              _size[1]=UINT16_MAX;
-              _size[2]=0;
-              _size[3]=UINT16_MAX;
-              _size[4]=0;
-            };
-            void post(uint row, uint col, ariel::Direction d,const std::string & str);
-            std::string read(uint row, uint col, ariel::Direction d, uint len);
-            void show();
-    };
+unordered_map<uint,uint> _size;
+unordered_map<uint,unordered_map<uint,char>> board;
+
+
+uint min_row=1;
+uint max_row=2;
+uint min_col=3;
+uint  max_col=4;
+
+  public:
+      Board(){
+        _size[1]=UINT16_MAX;
+        _size[2]=0;
+        _size[3]=UINT16_MAX;
+        _size[4]=0;
+      };
+      void post(uint row, uint col, ariel::Direction d,const string & str);
+    string read(uint row, uint col, ariel::Direction d, uint len);
+      void show();
+};
 }
